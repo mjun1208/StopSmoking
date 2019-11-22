642271b62de5a1ce0ef26d400f91df00c6b43767
mjun1208 <mjun1208@gmail.com>
2019-11-22 16:23:00 +0900

    no message

---
+++ Assets/Script/Mark.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mark : MonoBehaviour
+{
+    private Rigidbody2D rigid;
+    private Vector2 Dir;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        Dir = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        rigid.MovePosition(this.transform.position + (new Vector3(Dir.x, Dir.y , 0) * Time.deltaTime * 20));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Vector2 NormalVec = collision.contacts[0].normal;
+        Dir = Vector2.Reflect(Dir, NormalVec);
+    }
+}
